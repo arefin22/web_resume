1cb97c4ad2852836eda49a1f129156503d47b50f
arefin22 <shamsularefin500@gmail.com>
2023-12-12 10:53:27 +0600

    Contact Section Added

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,7 +1,7 @@
 
 const Footer = () => {
     return (
-        <div className="gradient-background rounded-2xl m-5 p-4 mb-0 text-center">
+        <div className="gradient-background rounded-2xl m-5 p-4 mb-0 text-black text-center">
             All Right Reserved @ 2023 | <a href="#home">SHAMSUL AREFIN</a>
         </div>
     );
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
 import Title from "../components/shared/Title";
 
 const Contact = () => {
-    const [formStatus, setFormStatus] = useState('Send')
     const onSubmit = (e) => {
         e.preventDefault()
-        setFormStatus('Submitting...')
         const { name, email, message } = e.target.elements
         let conFom = {
             name: name.value,
@@ -15,19 +12,20 @@ const Contact = () => {
         console.log(conFom)
     }
     return (
-        <div id="contact" className="gradient-background rounded-2xl m-5 p-4">
+        <div id="contact" className="gradient-background rounded-2xl m-5 p-4 py-24">
             <Title title="Contact Me"></Title>
 
             <div className="grid grid-cols-3 mx-auto px-8">
                 <div className="col-span-1">
-                    <h4 className="text-2xl my-48 text-black">Feel Free To Contact...</h4>
+                    <h4 className="text-2xl my-48 text-black">Feel free to <br /> reach me any time...</h4>
+                    {/* <p>Feel free to reach me any time.</p> */}
                 </div>
                 <form onSubmit={onSubmit} className="grid grid-cols-1 col-span-2 gap-3">
                     <input type="text" name="name" id="name" className="bg-white text-black p-4 rounded-2xl" placeholder="Write Your Name" />
                     <input type="email" name="email" className="bg-white text-black p-4 rounded-2xl" placeholder="example@mail.com" />
                     <input type="text" name="subject" className="bg-white text-black p-4 rounded-2xl" placeholder="Subject" />
                     <textarea name="content" className="bg-white text-black p-4 rounded-2xl" placeholder="Write Your Query Here..." id="" cols="30" rows="10"></textarea>
-                    <input type="submit" value="Send Now" className="btn btn-ghost btn-outline" />
+                    <input type="submit" value="Send Now" className="btn btn-ghost text-black hover:text-white btn-outline" />
                 </form>
             </div>
 
