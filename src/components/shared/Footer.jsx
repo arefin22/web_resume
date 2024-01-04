16b8b325fd95ac30e326e1c1361d8247a73fdb58
Shamsul Arefin <shamsularefin500@gmail.com>
2024-01-04 20:50:00 +0600

    Customized Background Image and Nav Section Accordingly.. Also Made Content area full

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,7 +1,7 @@
 
 const Footer = () => {
     return (
-        <div className="backdrop-blur-lg bg-white/10 rounded-2xl m-5 p-4 mb-0 text-black text-center">
+        <div className="backdrop-blur-lg bg-white/10 rounded-2xl m-5 mb-3 p-4 text-black text-center">
             All Right Reserved @ 2023 | <a href="#home">SHAMSUL AREFIN</a>
         </div>
     );
--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -6,38 +6,40 @@ const Navbar = () => {
     <div className="sticky top-5">
       <div className="backdrop-blur-lg drop-shadow-lg bg-white/30 rounded-2xl m-5 p-4">
         <img
-          className="w-72 rounded-2xl mx-auto"
+          className="w-72 rounded-2xl mx-auto drop-shadow-lg"
           src="https://i.ibb.co/qdhGT9x/f-66927.jpg"
           alt="Shamsul Arefin's Profile Photo"
         />
       </div>
       <div className="backdrop-blur-lg drop-shadow-lg bg-white/30 rounded-2xl m-5 p-4">
-        <h1 className="text-yellow-200  text-center text-4xl font-extrabold mt-4">
+        <h1 className="text-sky-900 text-center drop-shadow-lg text-4xl font-extrabold mt-4">
           Shamsul Arefin
         </h1>
         <p className="text-center text-sky-900">(Web Developer)</p>
         <hr className="m-2" />
-        <div className="flex justify-start items-center text-cyan-800 gap-3">
-          <FaPhoneSquareAlt />
-          <h3 className="text-xl font-medium">+880 1786 - 832 822</h3>
-        </div>
-        <div className="flex justify-start items-center text-cyan-800 gap-3">
-          <FaGithub />
-          <h3 className="text-xl font-medium">
-            <a href="https://github.com/arefin22">GitHub</a>
-          </h3>
-        </div>
-        <div className="flex justify-start text-cyan-800 items-center gap-3">
-          <FaLinkedin />
-          <h3 className="text-xl font-medium">
-            <a href="https://www.linkedin.com/in/shamsularefinn/">Linkedin</a>
-          </h3>
-        </div>
-        <div className="flex justify-start text-cyan-800 items-center gap-3">
-          <MdEmail />
-          <h3 className="text-xl font-medium">
-            <a href="mailto:shamularefin500@gmail.com"> Gmail</a>
-          </h3>
+        <div className="ml-5 ">
+          <div className="flex justify-start items-center text-cyan-800 gap-3">
+            <FaPhoneSquareAlt />
+            <h3 className="text-xl font-medium drop-shadow-lg">+880 1786 - 832 822</h3>
+          </div>
+          <div className="flex justify-start items-center hover:text-orange-200 text-cyan-800 gap-3">
+            <FaGithub />
+            <h3 className="text-xl drop-shadow-lg font-medium">
+              <a href="https://github.com/arefin22" target="_blank" rel="noopener noreferrer">GitHub</a>
+            </h3>
+          </div>
+          <div className="flex justify-start text-cyan-800 hover:text-orange-200 items-center gap-3">
+            <FaLinkedin />
+            <h3 className="text-xl drop-shadow-lg font-medium">
+              <a href="https://www.linkedin.com/in/shamsularefinn/" target="_blank" rel="noopener noreferrer">Linkedin</a>
+            </h3>
+          </div>
+          <div className="flex justify-start text-cyan-800 hover:text-orange-200 items-center gap-3">
+            <MdEmail />
+            <h3 className="text-xl drop-shadow-lg font-medium">
+              <a href="mailto:shamularefin500@gmail.com" target="_blank" rel="noopener noreferrer"> Gmail</a>
+            </h3>
+          </div>
         </div>
       </div>
 
